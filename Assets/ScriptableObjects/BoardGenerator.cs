f87cf0af8f69147c09e0f16b9a7bcffaadd11ab9
EilTon <dylan.delaunois@hotmail.com>
2019-11-20 10:28:41 +0100

    GenerateBoard

---
+++ Assets/ScriptableObjects/BoardGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardGenerator : MonoBehaviour
+{
+	int _CaseToGenerate;
+
+
+	public BoardGenerator(int caseToGenerate)
+	{
+		_CaseToGenerate = caseToGenerate;
+	}
+
+	public void GenerateBoard()
+	{
+		for(int i =0;i<_CaseToGenerate;++i)
+		{
+			Debug.Log("Case Generate");
+		}
+	}
+}
---
+++ Assets/ScriptableObjects/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public int _numberCase;
+
+	List<string> _cases;
+	BoardGenerator _boardGenerator;
+
+	private void Start()
+	{
+		_boardGenerator = new BoardGenerator(_numberCase);
+		_boardGenerator.GenerateBoard();
+	}
+}
