e7ae248c12043002e9fe9b1cc116dc9361fcbe5e
9640_gdev (Student) <s_9640_gdev@PEDAGOGIQUE.LAN>
2019-11-20 14:23:05 +0100

    test case + challenges

--- Assets/Scripts/Case.cs
+++ Assets/Scripts/Case.cs
@@ -6,4 +6,26 @@ public class Case : MonoBehaviour
 {
 	public ChallengeScriptableObject _challenge;
 	public List<Player> _players;
+
+	private void Start()
+	{
+		ChallengeToPlayer();
+	}
+
+	void ChallengeToPlayer()
+	{
+		if (_challenge._numberPlayer > 1)
+		{
+			string names ="";
+			foreach(var player in _players)
+			{
+				names = names + player.name + " ";
+			}
+			Debug.Log(names + _challenge._description.ToString());
+		}
+		else
+		{
+			Debug.Log(_players[0].name+ " " + _challenge._description.ToString());
+		}
+	}
 }
--- Assets/Scripts/ChallengeScriptableObject.cs
+++ Assets/Scripts/ChallengeScriptableObject.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 [CreateAssetMenu(fileName = "New Challenge",menuName ="Challenge/New Challenge")]
+
+
 public class ChallengeScriptableObject : ScriptableObject
 {
 	public string _category;
 	public string _description;
 	public int _points;
 	public bool _validation;
-	
+	public int _numberPlayer;
 }
