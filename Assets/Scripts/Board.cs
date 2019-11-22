8c698463b63cd2dbe329a65d77bbc8b27c2ead0a
EilTon <dylan.delaunois@hotmail.com>
2019-11-22 11:31:02 +0100

    MovePlayer

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -12,11 +12,14 @@ public class Board : MonoBehaviour
 	public List<GameObject> _players;
 	public float _offsetX = 1.5f;
 	int _order = 0;
+	bool _move = false;
+
 	private void Awake()
 	{
 		FindObjectOfType<GameManager>().generateBoard += GenerateBoardEventHandler;
 		FindObjectOfType<ButtonEventManager>().throwDice += ThrowDiceEventHandler;
 	}
+
 
 	void GenerateBoardEventHandler(object sender, GameManager.GenerateBoardEventArgs e) // fonction de l'event GenerateBoard
 	{
@@ -24,28 +27,29 @@ public class Board : MonoBehaviour
 		{
 			CreateCase(i);
 		}
-
+		_players = new List<GameObject>();
 		for (int i = 0; i < e.pseudosPlayer.Count; ++i)
 		{
 			CreatePlayer(e.pseudosPlayer[i], i);
 		}
-		_order = _players.Count - 1;
+		_order = 0;
 	}
 
-	public void ThrowDiceEventHandler(object sender, EventArgs e)
+	void ThrowDiceEventHandler(object sender, EventArgs e)
 	{
-		int numberOfDice = UnityEngine.Random.Range(1,7);
-		Vector3 move = new Vector3(numberOfDice, _players[_order].transform.position.y);
-		_players[_order].transform.Translate(move);
-		if(_order == 0)
+		float numberOfDice = UnityEngine.Random.Range(1, 7);
+		_players[_order].GetComponent<Player>().MovePlayer(numberOfDice);
+		if (_order >= _players.Count -1)
 		{
-			_order = _players.Count - 1;
+			_order = 0;
 		}
 		else
 		{
-			--_order;
+			++_order;
 		}
+		
 	}
+
 
 	void CreateCase(int i)
 	{
@@ -59,12 +63,12 @@ public class Board : MonoBehaviour
 
 	void CreatePlayer(string pseudo, int i)
 	{
-		Instantiate(_prefabPlayer);
-		_prefabPlayer.transform.position = new Vector2(0 - (i * _offsetX), 1);
-		Player player = _prefabPlayer.GetComponent<Player>();
+		GameObject playerGo = Instantiate(_prefabPlayer);
+		playerGo.transform.position = new Vector2(0, 1);
+		Player player = playerGo.GetComponent<Player>();
 		player._pseudo = pseudo;
-		_players.Add(_prefabPlayer);
-		_cases[0]._players.Add(_prefabPlayer);
+		_players.Add(playerGo);
+		_cases[0]._players.Add(playerGo);
 	}
 
 	ChallengeScriptableObject RandomChallenge()
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,13 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class Player : MonoBehaviour
 {
+
+	#region Declarations publique
+
 	public string _pseudo;
 	public int _score;
-	public List<string>_constraints;
+	public List<string> _constraints;
+
+	#endregion
+
+	#region Declarations prive
+
 	bool _isConstraint;
+
+	#endregion
+
+	public void MovePlayer(float numberOfDice)
+	{
+		Vector3 move = new Vector3((transform.position.x + numberOfDice) * 1.5f, 1, 0);
+		transform.position = move;
+	}
 
 }
